c96ae67bcaa8474cc0b2fd586bc19d96d8faf645
Yousha Rizvi <yousharizvi@gmail.com>
2021-03-19 07:15:40 +0500

    Error handling and a minor fix

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,28 +1,45 @@
 import styled from 'styled-components'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import Gist from './Gist'
 import { getPublicGists, getGistForUser } from '../services/gistService'
 import { debounce } from 'lodash';
 const GistList = ({ searchKeyword }) => {
+  const [errorMessage, setErrorMessage] = useState('')
   const [gistsData, setGistsData] = useState([])
-  const debouncedGetGistsData = debounce(() => {
-    getGistsData()
-  }, 2000)
-  const getGistsData = async () => {
+  const debouncedGetGistsData = useCallback(debounce((_searchKeyword) => {
+    getGistsData(_searchKeyword)
+  }, 500), [])
+  const getGistsData = async (_searchKeyword) => {
     let response
-    if (searchKeyword) response = await getGistForUser(searchKeyword)
-    else response = await getPublicGists()
-    if (response.status === 200) setGistsData(response.data)
-    else console.log(response.status)
+    setGistsData([])
+    setErrorMessage('')
+    try {
+      if (_searchKeyword) response = await getGistForUser(_searchKeyword)
+      else response = await getPublicGists()
+      if (response.status === 200) setGistsData(response.data)
+      else console.log(response.status)
+    } catch (e) {
+      setErrorMessage(e.message || 'No gists found for this username')
+    }
   }
-  useEffect(() => {
-    // getGistsData()
-    debouncedGetGistsData()
+  useEffect((value) => {
+    debouncedGetGistsData(searchKeyword)
   }, [searchKeyword])
+
   return <GistListWrapper>
+    {errorMessage ? <ErrorMessage>
+      {errorMessage}
+    </ErrorMessage> : ''}
     {gistsData.map(gist => <Gist gist={gist} key={gist.id} />)}
   </GistListWrapper>
 }
+
+const ErrorMessage = styled.div`
+  align-self: center;
+  text-align: center;
+  font-size: 1.2em;
+  padding: 20px 0;
+`;
 
 const GistListWrapper = styled.div`
   display: flex;
