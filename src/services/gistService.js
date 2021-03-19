88184d97aa206e2be04d8c0c54aff601621c90aa
Yousha Rizvi <yousharizvi@gmail.com>
2021-03-19 07:41:46 +0500

    Apply caching to user api

--- src/services/gistService.js
+++ src/services/gistService.js
@@ -1,6 +1,16 @@
 import { Octokit } from "@octokit/rest";
+import ls from "localstorage-ttl";
+
 const octokit = new Octokit()
 
 export const getPublicGists = () => octokit.gists.listPublic()
 
-export const getGistForUser = username =>  octokit.gists.listForUser({ username })
+export const getGistForUser = username => {
+  const cachedResponse = ls.get(username)
+  if (cachedResponse) return Promise.resolve(cachedResponse)
+  return octokit.gists.listForUser({ username })
+    .then(response => {
+      ls.set(username, response, 60 * 1000)
+      return response
+    })
+}
